86ca499bde76424f3c12ceba679a0f5f4a8dd10d
Faal <23237417+Faaly@users.noreply.github.com>
2023-09-13 09:34:05 +0200

    Update hello-world.cpp

--- hello-world.cpp
+++ hello-world.cpp
@@ -1,1 +1,16 @@
 
+#include <iostream>
+#include <string>
+
+std::string say_hello();
+
+int main(){
+  std::cout << say_hello() << std::endl;
+  return 0;
+}
+
+std::string say_hello(){
+  std::string hello {"Hello World!"};
+  return hello;
+}
+
